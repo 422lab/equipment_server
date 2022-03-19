1a6bcac4880de8bc7db7bce0291b6c6f6c28ef85
823984418 <823984418@qq.com>
2022-03-19 13:49:22 +0800

    登录提示

--- src/main/webapp/web/script/index.js
+++ src/main/webapp/web/script/index.js
@@ -7,14 +7,18 @@ async function login(event) {
     let account = document.getElementById("account").value;
     let password = document.getElementById("password").value;
     let bridge = new ServerBridge(account, password);
+    let hit = document.getElementById("loginHit");
+    document.getElementById("failHit").style.visibility = "hidden";
+    hit.style.display = "inline-block";
     let result = await bridge.userLogin();
+    hit.style.display = "none";
     if (result.code === 0) {
         sessionStorage.setItem("account", account);
         sessionStorage.setItem("password", password);
         location.href = "user.html";
         return;
     }
-    alert("登录失败");
+    document.getElementById("failHit").style.visibility = "visible";
 }
 
 window.onload = function onload(event) {
